a3ef93c05a614b7f5d983107889c9a13a8fb85a3
Oleksii Kozhemiaka <olesis.ko@gmail.com>
2023-04-16 23:55:24 +0300

    Initial commit

---
+++ ConsoleApp1/BenchmarkTest.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ConsoleApp1;
+
+[MemoryDiagnoser()]
+public class BenchmarkTest
+{
+    int[] sequence = { 1, 2, 3 };
+    int[] array= new int[10000000]; 
+    Random rnd = new Random();
+    [GlobalSetup]
+    public void Setup()
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(1, 10);
+        }
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            array[rnd.Next(0, array.Length)] = sequence[i];
+        }
+    }
+
+    [Benchmark]
+    public void TestWithParallel()
+    {
+        int count = 0;
+        Parallel.For(0, array.Length - sequence.Length + 1, i =>
+        {
+            bool found = true;
+            for (int j = 0; j < sequence.Length; j++)
+            {
+                if (array[i + j] != sequence[j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Interlocked.Increment(ref count);
+            }
+        });
+    }
+
+    [Benchmark]
+    public void TestWithoutParallel()
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length - sequence.Length + 1; i++)
+        {
+            bool found = true;
+            for (int j = 0; j < sequence.Length; j++)
+            {
+                if (array[i + j] != sequence[j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (found)
+            {
+                count++;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using ConsoleApp1;
+
+var summary = BenchmarkRunner.Run<BenchmarkTest>();
+
+            // int[] sequence = { 1, 2, 3 };
+            // //write array of 1000 elements with 1,2,3,4,5 in random places
+            // int[] array = new int[1000000];
+            // Random rnd = new Random();
+            // for (int i = 0; i < array.Length; i++)
+            // {
+            //     array[i] = rnd.Next(1, 10);
+            // }
+            // for (int i = 0; i < sequence.Length; i++)
+            // {
+            //     array[rnd.Next(0, array.Length)] = sequence[i];
+            // }
+            // //end of write array of 1000 elements with 1,2,3,4,5 in random places
+            // int count = 0;
+            //
+            // Parallel.For(0, array.Length - sequence.Length + 1, i =>
+            // {
+            //     bool found = true;
+            //     for (int j = 0; j < sequence.Length; j++)
+            //     {
+            //         if (array[i + j] != sequence[j])
+            //         {
+            //             found = false;
+            //             break;
+            //         }
+            //     }
+            //     if (found)
+            //     {
+            //         Console.WriteLine("Sequence found by thread {0} at index {1}", Task.CurrentId, i);
+            //         System.Threading.Interlocked.Increment(ref count);
+            //     }
+            // });
+            //
+            // Console.WriteLine("Total number of occurrences: {0}", count);
